6b7062dbfe259a2c6d858d2d43c51d3e8510afdb
ASUS <ASUS@LAPTOP-06IB4115>
2021-01-10 13:09:01 +0530

    [pratik]:update has done by me.

--- src/demo/demo1.java
+++ src/demo/demo1.java
@@ -4,6 +4,7 @@ public class demo1 {
 public static void main(String[] args) {
 	
 	System.out.println("pratik,vaibhav,bhagyshri all are selected in january"); 
+	System.out.println("All are got 10 lakh package");
 	
 }
 }
